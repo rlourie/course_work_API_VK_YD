b87031914cab8097c9cb9cd2b3d0fd1ab0902030
rlourie <super.sanek20142@yandex.ru>
2021-11-10 10:58:22 +0300

    V1.1

--- main.py
+++ main.py
@@ -36,13 +36,33 @@ class VkUser:
             likes_url_dict["date"] = date
             json_list.append(likes_url_dict)
             likes_url_dict = {}
-        pprint(json_list)
-        with open("data_file.json", "w") as write_file:
-            json.dump(json_list, write_file)
-            print(write_file)
-        return 0
+        return json_list
+
+
+# В ключ file_name кладем правильное значение, убираем дубли.
+# Возвращает инфомацию с url и file_name
+def work_json_dict(list_of_dictionaries):
+    for dict_check in list_of_dictionaries:
+        for dict in list_of_dictionaries:
+            if (dict_check["file_name"] == dict["file_name"]) and (dict_check["url"] != dict["url"]):
+                dict_check["file_name"] = dict_check["file_name"] + "_" + "(" + dict_check["date"] + ")"
+                dict["file_name"] = dict["file_name"] + "_" + "(" + dict["date"] + ")"
+    return list_of_dictionaries
+
+
+# Возвращает итоговый json и создает его
+def correct_json(json_dict):
+    buf = json_dict
+    for dict in buf:
+        del dict["date"]
+        del dict["url"]
+    with open("data_file.json", "w") as write_file:
+        json.dump(buf, write_file, indent=5)
+    return buf
 
 
 Alex = VkUser("552934290")
 
-pprint(Alex.get_photos(50))
+pprint(correct_json(work_json_dict(Alex.get_photos(50))))
+print()
+pprint(work_json_dict(Alex.get_photos(50)))
