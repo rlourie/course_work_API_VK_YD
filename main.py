dfb9fcfeac37ef1fdb3e28323e24e70ff13715ee
rlourie <super.sanek20142@yandex.ru>
2021-11-11 00:12:55 +0300

    V1.3

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ from datetime import datetime
 
 
 class VkUser:
-    def __init__(self, id):
+    def __init__(self, vk_id):
         self.url = "https://api.vk.com/method/"
         self.token = "958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008"
-        self.id = id
+        self.id = vk_id
         self.params = {"access_token": self.token,
                        "v": "5.131",
                        "owner_id": self.id
                        }
 
-    def get_photos(self, count):
+    def get_photos(self, count_photos):
         json_list = []
         likes_url_dict = {}
         get_photos_params = {
             "album_id": "profile",
             "extended": "1",
-            "count": count,
+            "count": count_photos,
             "photo_sizes": "1"
         }
         get_photos_url = self.url + "photos.get"
@@ -40,26 +40,27 @@ class VkUser:
 
 
 class YaUploader:
-    def __init__(self, upload_list, token, id_vk):
+    def __init__(self, upload_list, token, vk_id):
         self.token = token
         self.upload_list = upload_list
         self.url = "https://cloud-api.yandex.net/v1/disk/resources/"
         self.headers = {'Authorization': self.token}
-        self.id = id_vk
+        self.id = vk_id
 
+    # Загружает фотографии и созадет папку с проверками
     def upload(self):
         check_list = self.get_list_name_photos()
         count_upload_photos = 0
-        dir = requests.put(self.url, headers=self.headers, params={"path": self.id})
-        if dir.status_code == 201 or dir.status_code == 409:
-            if dir.status_code == 201:
+        dir_create = requests.put(self.url, headers=self.headers, params={"path": self.id})
+        if dir_create.status_code == 201 or dir_create.status_code == 409:
+            if dir_create.status_code == 201:
                 print(f"Папка с именем {self.id} созданна")
             else:
                 print(f"Папка с именем {self.id} уже созданна")
-            for dict in self.upload_list:
-                buf = dict["file_name"] + ".jpeg"
-                file_name = self.id + "/" + dict["file_name"] + ".jpeg"
-                url_photo = dict["url"]
+            for dict_params in self.upload_list:
+                buf = dict_params["file_name"] + ".jpeg"
+                file_name = self.id + "/" + dict_params["file_name"] + ".jpeg"
+                url_photo = dict_params["url"]
                 params = {"path": file_name,
                           "url": url_photo}
                 if not (buf in check_list):
@@ -72,15 +73,16 @@ class YaUploader:
                 else:
                     print(f"Файл с именем {buf} уже существует")
         else:
-            print(f"ошибка {dir.status_code}")
+            print(f"ошибка {dir_create.status_code}")
         print(f"Загруженно фотографий: {count_upload_photos}")
         print("Завершенно")
 
+    # Получает весь список фотографий с YD
     def get_list_name_photos(self):
         result_list = []
         response = requests.get(self.url + "files", headers=self.headers).json()
-        for dict in response["items"]:
-            name = dict["name"]
+        for dict_params in response["items"]:
+            name = dict_params["name"]
             result_list.append(name)
         return result_list
 
@@ -89,20 +91,20 @@ class YaUploader:
 # Возвращает инфомацию с url и file_name
 def work_json_dict(list_of_dictionaries):
     for dict_check in list_of_dictionaries:
-        for dict in list_of_dictionaries:
-            if (dict_check["file_name"] == dict["file_name"]) and (dict_check["url"] != dict["url"]):
+        for dict_params in list_of_dictionaries:
+            if (dict_check["file_name"] == dict_params["file_name"]) and (dict_check["url"] != dict_params["url"]):
                 dict_check["file_name"] = dict_check["file_name"] + "_" + "(" + dict_check["date"] + ")"
-                dict["file_name"] = dict["file_name"] + "_" + "(" + dict["date"] + ")"
+                dict_params["file_name"] = dict_params["file_name"] + "_" + "(" + dict_params["date"] + ")"
     return list_of_dictionaries
 
 
 # Возвращает итоговый json и создает его
 def correct_json(json_dict):
     buf = json_dict
-    for dict in buf:
-        dict['file_name'] += ".jpeg"
-        del dict["date"]
-        del dict["url"]
+    for dict_params in buf:
+        dict_params['file_name'] += ".jpeg"
+        del dict_params["date"]
+        del dict_params["url"]
     with open("data_file.json", "w") as write_file:
         json.dump(buf, write_file, indent=5)
     return buf
@@ -110,9 +112,10 @@ def correct_json(json_dict):
 
 id_vk = "552934290"
 token_YD = "XXX"
+count = 50
 
 Alex = VkUser(id_vk)
-get_photos = Alex.get_photos(50)
+get_photos = Alex.get_photos(count)
 work_dict = work_json_dict(get_photos)
 
 uploader = YaUploader(work_dict, token_YD, id_vk)
